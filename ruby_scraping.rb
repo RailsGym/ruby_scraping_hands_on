f994e022dde2fcb80db800b5a93d0a5565d7f7c6
michiwo1 <mk1104kado@gmail.com>
2021-06-17 11:56:27 +0900

    Qittaのホームページをスクレイピング

---
+++ ruby_scraping.rb
@@ -0,0 +1,45 @@
+
+# webに接続するためのライブラリ
+require "open-uri"
+# htmlの情報を取得するためのライブラリ
+require "nokogiri"
+# csvを出力するためのライブラリ
+require 'csv'
+ 
+# クレイピング対象のURL
+url = "https://qiita.com/"
+
+# 取得するhtml用charset
+charset = nil
+ 
+html = open(url) do |page|
+  # charsetを自動で読み込み、取得
+  charset = page.charset
+  # 中身を読む
+  page.read
+end
+ 
+# Nokogiri で切り分け
+contents = Nokogiri::HTML.parse(html,nil,charset)
+
+# <br>タグを改行（\n）に変えて置くとスクレイピングしやすくなるらしい。
+contents.search('br').each { |n| n.replace("\n") }
+
+titles = []
+contents.xpath("//h2/a").map do |title|
+  titles.push(title.text)
+end
+
+detail_urls = []
+contents.xpath("//h2/a").map do |url|
+  detail_urls.push(url.attribute('href').value)
+end
+
+rows = [titles, detail_urls]
+
+# csvで出力
+CSV.open('target.csv', 'w') do |csv|
+  rows.each do |row|
+    csv << row
+  end
+end
